6e9ac00edf666aa89d8760755a892af38faa17c6
C Hamilton <hamiltoncj@users.noreply.github.com>
2023-08-02 13:33:28 +0000

    Undoing previous changes.

--- bulkNominatim.py
+++ bulkNominatim.py
@@ -77,12 +77,8 @@ class BulkNominatim(object):
         self.reverseGeocodeTool.unload()
     
     def setReverseGeocodeTool(self):
-        if self.reverseGeocodeAction.isChecked():
-            self.reverseGeocodeAction.setChecked(False)
-            self.canvas.unsetMapTool(self.reverseGeocodeTool)
-        else:
-            self.reverseGeocodeAction.setChecked(True)
-            self.canvas.setMapTool(self.reverseGeocodeTool)
+        self.reverseGeocodeAction.setChecked(True)
+        self.canvas.setMapTool(self.reverseGeocodeTool)
         
     def nominatimTool(self):
         """Display the dialog window."""
